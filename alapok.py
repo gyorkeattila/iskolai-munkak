60a0e1b1fbee9e31feeea7dd1f70abf83a402fd2
gyorkeattila <71698318+gyorkeattila@users.noreply.github.com>
2021-09-20 09:40:57 +0200

    Create alapok.py

---
+++ alapok.py
@@ -0,0 +1,67 @@
+import random
+
+print('Heló üdvözöllek!')
+print('Szia uram')
+print(9)
+print(15+5)
+print(2+3*50000000000)
+print(15/4)
+print(15/1)
+print(15 % 7) #osztásimaradék
+#szevaszfiam
+
+print('Szeretem a tejet')
+print(2**3) #hatványozás
+print(12 // 3)
+print(12.3 * 2)
+
+print('You are watching a master at work')
+
+print("3+5", 3+5 )
+print("al" , "ma")
+
+print("Szia" ,end="") #soremelés elnyomás
+print("Uram")
+
+print("ez\nnagy\nalma") #több soros kiírás mert az jó 
+print("ez\tnagy\talma")
+
+print('( ͡° ͜ʖ ͡°)')
+
+print('aa\bb\cc\dd\ee\ff\gg\hh\ii\jj\kk\ll\mm')
+print('ezt mondta \'Szép vagy\' - Pista')
+
+
+
+szam=12
+print(szam)
+szam="Alma sus"
+print(szam)
+szam=9
+x=1/2
+print(szam ** x)
+print(27 ** x)
+print( (-4) ** 1/2) #hibás művelet!!!!!!!!!!!!!!
+print( 5 ** -1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
