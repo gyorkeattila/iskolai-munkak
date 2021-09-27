4a993279c9499199bea996abb77c482e64288b17
gyorkeattila <71698318+gyorkeattila@users.noreply.github.com>
2021-09-27 09:40:18 +0200

    Create userInput.py

---
+++ userInput.py
@@ -0,0 +1,13 @@
+'''
+print('Kérek egy számot:', end='')
+be=input()
+print(be)
+'''
+
+vezeteknev = input(" Mi a vezetekneved? ")
+keresztnev = input ( " Mi a keresztneved? ")
+print('Na csumi '+vezeteknev+" "+keresztnev)
+
+kor=input("Hány éves vagy?:")
+print("Na csumi "+kor+" "+vezeteknev+" "+keresztnev)
+print("Na csumi ",kor,vezeteknev,keresztnev)
